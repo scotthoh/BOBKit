8dc32e6ad5b2a262f466ae6aee7b90dce4708c0f
Soon Wen Hoh <soonwen.hoh@york.ac.uk>
2024-08-02 17:58:12 +0100

    add bindings to b2u and u2b functions

--- python/c_util.cpp
+++ python/c_util.cpp
@@ -7,6 +7,8 @@ using namespace clipper;
 void init_clipper_util(py::module &m) {
   py::class_<Util>(m, "Util")
       .def(py::init<>())
+      .def_static("u2b", Util::u2b, py::arg("x"))
+      .def_static("b2u", Util::b2u, py::arg("x"))
       .def_static("is_nan", static_cast<bool (*)(const ftype32)>(&Util::is_nan))
       .def_static("is_nan",
                   static_cast<bool (*)(const ftype64)>(&Util::is_nan));
